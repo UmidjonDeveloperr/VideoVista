20557b593788dbdfbfaaf59fa159f460a83464f5
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-05-09 11:22:43 +0500

    INFRA: Initialize Api Project

---
+++ VideoVista.Api/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace VideoVista.Api.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class HomeController : RESTFulController
+	{
+		[HttpGet]
+		public ActionResult<string> Get() =>
+			Ok("Hello Mario, the princess is in another castle");
+	}
+}
