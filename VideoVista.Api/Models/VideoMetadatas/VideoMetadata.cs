1bc19b6b8a9a1f5c9a978756f00cb667c038ce7d
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-05-09 12:45:48 +0500

    DATA: Add Video Metadata

---
+++ VideoVista.Api/Models/VideoMetadatas/VideoMetadata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VideoVista.Api.Models.VideoMetadatas
+{
+	public class VideoMetadata
+	{
+		public Guid Id { get; set; }
+		public string Title { get; set; }
+		public string Discription { get; set; }
+		public string BlobPath { get; set; }
+		public string ThubNail { get; set; }
+		public DateTimeOffset CreatedDate { get; set; }
+		public DateTimeOffset UpdatedDate { get; set; }
+	}
+}
