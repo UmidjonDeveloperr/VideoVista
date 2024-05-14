c9d73f51cd65024e4c2fb8156f7cab13ebcf530d
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-05-14 15:58:55 +0500

    SERVICES: Implement Crud Operations

---
+++ VideoVista.Api/Services/VideoMetadatas/IVideoMetadataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using VideoVista.Api.Models.VideoMetadatas;
+
+namespace VideoVista.Api.Services.VideoMetadatas
+{
+	public interface IVideoMetadataService
+	{
+		ValueTask<VideoMetadata> AddVideoMetadataAsync(VideoMetadata videoMetadata);
+		IQueryable<VideoMetadata> RetrieveAllVideoMetadatas();
+		ValueTask<VideoMetadata> RetrieveVideoMetadataByIdAsync(Guid Id);
+		ValueTask<VideoMetadata> ModifyVideoMetadataAsync(VideoMetadata videoMetadata);
+		ValueTask<VideoMetadata> RemoveVideoMetadataAsync(Guid guid);
+	}
+}
