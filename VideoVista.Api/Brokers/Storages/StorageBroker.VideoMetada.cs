9e54c788d2110e84568ffd5aa8ed276c70b8b471
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-05-14 15:27:25 +0500

    BROKERS: Implement Crud Functionalities

--- VideoVista.Api/Brokers/Storages/StorageBroker.VideoMetada.cs
+++ VideoVista.Api/Brokers/Storages/StorageBroker.VideoMetada.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 using VideoVista.Api.Models.VideoMetadatas;
 
 namespace VideoVista.Api.Brokers.Storages
@@ -6,5 +9,20 @@ namespace VideoVista.Api.Brokers.Storages
 	public partial class StorageBroker
 	{
 		public DbSet<VideoMetadata> VideoMetadatas { get; set; }
+
+		public async ValueTask<VideoMetadata> InsertVideoMetadataAsync(VideoMetadata videoMetadata) =>
+			await InsertAsync(videoMetadata);
+
+		public IQueryable<VideoMetadata> SelectAllVideoMetadatas() =>
+			SelectAll<VideoMetadata>();
+
+		public async ValueTask<VideoMetadata> SelectVideoMetadataAsync(Guid Id) =>
+			await SelectAsync<VideoMetadata>(Id);
+
+		public async ValueTask<VideoMetadata> UpdateVideoMetadataAsync(VideoMetadata videoMetadata) =>
+			await UpdateAsync(videoMetadata);
+
+		public async ValueTask<VideoMetadata> DeleteVideoMetadataAsync(VideoMetadata videoMetadata) =>
+			await DeleteAsync(videoMetadata);
 	}
 }
