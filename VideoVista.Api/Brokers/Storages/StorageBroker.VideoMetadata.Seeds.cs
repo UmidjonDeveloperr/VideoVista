c209e2d38e2b171592c3b551bea6ca51cbf28a51
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-05-15 20:05:35 +0500

    BROKERS: Initialize Seed Data

---
+++ VideoVista.Api/Brokers/Storages/StorageBroker.VideoMetadata.Seeds.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using VideoVista.Api.Models.VideoMetadatas;
+
+namespace VideoVista.Api.Brokers.Storages
+{
+	public partial class StorageBroker
+	{
+		private static void SeedVideoMetadata(EntityTypeBuilder<VideoMetadata> builder)
+		{
+			builder.HasData(new VideoMetadata
+			{
+				Id = Guid.Parse("47729a8b-e359-493e-a982-e7c818cd1220"),
+				Title = "Title",
+				Discription = "FirstDiscription",
+				BlobPath = "path",
+				ThubNail = "thubnail",
+				CreatedDate = DateTimeOffset.Now,
+				UpdatedDate = DateTimeOffset.Now
+			});
+		}
+	}
+}
---
+++ VideoVista.Api/Migrations/20240515145611_InitialSeedData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VideoVista.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "VideoMetadatas",
+                columns: new[] { "Id", "BlobPath", "CreatedDate", "Discription", "ThubNail", "Title", "UpdatedDate" },
+                values: new object[] { new Guid("47729a8b-e359-493e-a982-e7c818cd1220"), "path", new DateTimeOffset(new DateTime(2024, 5, 15, 19, 56, 10, 58, DateTimeKind.Unspecified).AddTicks(5563), new TimeSpan(0, 5, 0, 0, 0)), "FirstDiscription", "thubnail", "Title", new DateTimeOffset(new DateTime(2024, 5, 15, 19, 56, 10, 58, DateTimeKind.Unspecified).AddTicks(5591), new TimeSpan(0, 5, 0, 0, 0)) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "VideoMetadatas",
+                keyColumn: "Id",
+                keyValue: new Guid("47729a8b-e359-493e-a982-e7c818cd1220"));
+        }
+    }
+}
