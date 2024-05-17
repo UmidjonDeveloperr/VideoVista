62e50c8e54b328b1c416f2fe6f3628d100b18440
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-05-17 17:34:08 +0500

    BROKERS: Added Logging Brokers

---
+++ VideoVista.Api.Tests.Unit/Services/VideoMetadatas/VideoMetadataServiceTests.cs
@@ -0,0 +1,13 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoVista.Api.Tests.Unit.Services.VideoMetadatas
+{
+	public partial class VideoMetadataServiceTests
+	{
+	}
+}
