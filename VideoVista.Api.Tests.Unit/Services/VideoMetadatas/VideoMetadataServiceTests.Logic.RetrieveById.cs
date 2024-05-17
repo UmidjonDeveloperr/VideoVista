7efe84f39e77fe4ce8aab7875c4a8d9541057419
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-05-17 18:39:05 +0500

    ShouldRetrieveVideoMetadataById -> Fail

---
+++ VideoVista.Api.Tests.Unit/Services/VideoMetadatas/VideoMetadataServiceTests.Logic.RetrieveById.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoVista.Api.Models.VideoMetadatas;
+using Xunit;
+
+namespace VideoVista.Api.Tests.Unit.Services.VideoMetadatas
+{
+	public partial class VideoMetadataServiceTests
+	{
+		[Fact]
+		public async Task ShouldRetrieveVideoMetadataById()
+		{
+			//given
+			Guid randomId = Guid.NewGuid();
+			VideoMetadata randomVideoMetadata = CreateRandomVideoMetadata();
+			VideoMetadata storageVideoMetadata = randomVideoMetadata;
+			VideoMetadata expectedVideoMetadata = storageVideoMetadata;
+
+			this.storageBrokerMock.Setup(broker =>
+				broker.SelectVideoMetadataByIdAsync(randomId))
+					.ReturnsAsync(storageVideoMetadata);
+
+			//when
+			VideoMetadata actualVideoMetadata = 
+				await this.videoMetadataService.RetrieveVideoMetadataByIdAsync(randomId);
+
+			//then
+			actualVideoMetadata.Should().BeEquivalentTo(expectedVideoMetadata);
+
+			this.storageBrokerMock.Verify(broker =>
+				broker.SelectVideoMetadataByIdAsync(randomId), Times.Once);
+
+			this.storageBrokerMock.VerifyNoOtherCalls();
+			this.loggingBrokerMock.VerifyNoOtherCalls();
+		}
+	}
+}
